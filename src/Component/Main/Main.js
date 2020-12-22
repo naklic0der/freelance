9f6b0bd4f71eebce3527e88db770c309df7f4d0b
naklic0der <naklicoder@gmail.com>
2020-12-22 12:56:26 +0530

    added framer motion

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,15 +1,16 @@
 import React, {useEffect} from 'react'
 import {gsap, Power4} from "gsap";
-import {connect} from "react-redux";
 import style from './Main.module.sass'
 import Aux from "../../hoc/Aux";
+import {AnimatePresence, motion} from "framer-motion"
+import {connect} from "react-redux";
 
 const Main = (props) => {
 
    const go_left = () => {
       props.upd_cur_loc('left')
-      gsap.to('div h1', {
-         x: 250,
+      gsap.to('#_h1', {
+         x: 600,
          duration: 2,
          ease: Power4.easeOut,
       })
@@ -17,12 +18,13 @@ const Main = (props) => {
 
    const go_main = () => {
       props.upd_cur_loc('main')
-      gsap.to('div h1', {
+      gsap.to('#_h1', {
          x: 0,
          duration: 2,
          ease: Power4.easeOut,
       })
    }
+
    useEffect(() => {
       console.log(props.current_location)
    })
@@ -30,24 +32,49 @@ const Main = (props) => {
    return (
       <div>
          {/*Common*/}
-         <h1 className={style.h1}>Udit Sethi</h1>
+         <h1 className={style.h1} id={'_h1'}>Udit Sethi</h1>
 
          {/* Main Page*/}
-         {props.current_location === 'main' && (
-            <button onClick={() => go_left()}>Left</button>
-         )}
+         <AnimatePresence>
+            {props.current_location === 'main' && (
+               <button
+                  initial={{opacity: 0}}
+                  animate={{opacity: 1}}
+                  exit={{opacity: 0}}
+                  onClick={() => go_left()} className={style.go_left}>Left</button>
+            )}
+         </AnimatePresence>
 
          {/* Left Page */}
-         {props.current_location === 'left' && (
-            <Aux>
-               <button onClick={() => go_main()}>Main</button>
-               <p>Hey i am on left page</p>
-            </Aux>
-         )}
+         <AnimatePresence>
+            {props.current_location === 'left' && (
+               <Aux>
+                  <motion.div
+                     initial={{x: -300, opacity: 1}}
+                     animate={{x: 0}}
+                     exit={{x: -500}}
+                     className={style.white_card}
+                  >
+                     This is white card
+                  </motion.div>
+                  <motion.button
+                     initial={{opacity: 0}}
+                     animate={{opacity: 1}}
+                     exit={{opacity: 0}}
+                     onClick={() => go_main()} className={style.go_main}>Main
+                  </motion.button>
+                  <motion.p
+                     initial={{opacity: 0}}
+                     animate={{opacity: 1}}
+                     exit={{opacity: 0}}
+                  >Hey i am on left page
+                  </motion.p>
+               </Aux>
+            )}
+         </AnimatePresence>
       </div>
    )
 }
-
 // REDUX STUFF
 
 // GET SLICE OF STATE
